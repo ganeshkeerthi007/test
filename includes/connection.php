88aa9866d5e5480cb65ef7025735b274ed0cab4a
ganeshkeerthi007 <104976963+ganeshkeerthi007@users.noreply.github.com>
2022-05-06 14:33:01 -0400

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,7 @@
 <?php
-$con = mysqli_init();mysqli_real_connect($conn, "azure@localhost", "azure", "6#vWHD_$", "emp", 3306, MYSQLI_CLIENT_SSL);
+$port = $_SERVER['WEBSITE_MYSQL_PORT'];
+$server = "localhost:$port";  
+
+
+$con = mysqli_init();mysqli_real_connect($conn, $server, "azure", "6#vWHD_$", "localdb", 3306, MYSQLI_CLIENT_SSL);
 ?>
