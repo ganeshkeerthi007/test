9874675ab0772ddb25f09da77c353702423fd925
ganeshkeerthi007 <104976963+ganeshkeerthi007@users.noreply.github.com>
2022-05-06 06:58:29 -0400

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_init();mysqli_real_connect($conn, "test2-social.mysql.database.azure.com", "mysocial", "Mysocial@007", "emp", 3306, MYSQLI_CLIENT_SSL);
+$con = mysqli_init();mysqli_real_connect($conn, "azure@localhost", "mysocial", "Mysocial@007", "emp", 3306, MYSQLI_CLIENT_SSL);
 ?>
