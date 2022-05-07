cee0970f319fd5bea4be44186e0d3076fd077282
ganeshkeerthi007 <104976963+ganeshkeerthi007@users.noreply.github.com>
2022-05-07 05:46:04 -0400

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 $port = $_SERVER['WEBSITE_MYSQL_PORT'];
 $server = "localhost:$port";  
-$con = mysqli_init();mysqli_real_connect($server, "azure", "6#vWHD_$", "localdb", 3306, MYSQLI_CLIENT_SSL);
-$ pip install -r requirements.txt --proxy address:$port
+$con = mysqli_init();mysqli_real_connect($server, "azure", "6#vWHD_$", "localdb");
+//$ pip install -r requirements.txt --proxy address:$port
 ?>
