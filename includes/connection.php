017dc09428012b76ceb4e0ef1746ed4e072aadbd
ganeshkeerthi007 <104976963+ganeshkeerthi007@users.noreply.github.com>
2022-05-08 11:12:46 -0400

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 $port = $_SERVER['WEBSITE_MYSQL_PORT'];
 $server = "localhost:$port";  
-$con =mysqli_connect($server, "azure", "6#vWHD_$", "localdb");
+$con = mysqli_init();mysqli_real_connect($server, "azure", "6#vWHD_$", "localdb");
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 } 
